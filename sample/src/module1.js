00ace07e77111f0af43b0d33cc2bc23d5793ff59
vishnugitit <vishnujangam1527@gmail.com>
2024-12-12 14:04:44 +0530

    final commit

---
+++ sample/src/module1.js
@@ -0,0 +1,20 @@
+import './module.css'
+function Module1(){
+    return(
+        <>
+<div className="container">
+    <h1 className="blackhead">I'm a Full Stack</h1>
+    <h1 className="bluehead">
+Web
+    </h1>
+    <h1 className="blackhead">
+Developer
+    </h1>
+    <p className="red">"Being a Full-Stack_developer is not knowing everything- it's about knowing how to learn and adapt"</p>
+    <p className="blue">"Every bug you fix takes you one step closer to mastering the stack"</p>
+    <p className="black">"The best code is no code at all"</p>
+</div>
+        </>
+    )
+}
+export default Module1;
